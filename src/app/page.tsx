9906914f9bfa677fcc5e788b8ce281d2ade621ab
Zach Sanders <sanders.zach@gmail.com>
2025-01-29 22:29:24 -0500

    Removing code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 import { Button } from '@chakra-ui/react';
 import { Button as ComponentsButton } from "@orders/components";
-import type { ComponentType } from 'react';
 
 export default function Home() {
-  // const ButtonComponent = ComponentsButton as ComponentType<{ label: string }>;
   return (
     <div style={{ padding: "20px" }}>
       <h1>Welcome to Orders</h1>
